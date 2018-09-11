df190eea2951540ad13156c3cd6044362157a7dd
g3n35i5 <janfschmidt@me.com>
2018-09-11 15:50:24 +0200

    Making the pagenotfound component work.

--- src/app/pagenotfound/pagenotfound.component.ts
+++ src/app/pagenotfound/pagenotfound.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-pagenotfound',
   templateUrl: './pagenotfound.component.html',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PagenotfoundComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
 
+  /** Return to the root url. */
+  return () {
+    this.router.navigate(['/']);
+  }
 }
