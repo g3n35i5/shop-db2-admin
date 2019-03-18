38e23ce5f719502b677b2364c9640cb66528d60e
g3n35i5 <janfschmidt@me.com>
2019-03-18 10:54:05 +0100

    UI Tweaks

--- src/app/filters.ts
+++ src/app/filters.ts
@@ -21,7 +21,7 @@ export class LocalTimezone implements PipeTransform {
 
 @Pipe({ name: 'customTimestamp' })
 export class CustomTimestamp implements PipeTransform {
-  transform(input: string, format = 'DD.MM.YYYY HH:mm:ss'): string {
+  transform(input: string, format = 'ddd, DD MMM YYYY HH:mm'): string {
     if (typeof(input) === 'undefined' || input === null) {
       return 'unknown';
     }
