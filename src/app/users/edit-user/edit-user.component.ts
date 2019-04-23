f7cb797f99739bea212ced8407311a65c947c404
g3n35i5 <janfschmidt@me.com>
2019-04-23 08:42:27 +0200

    Edit user: Removing the required tag from the firstname field.

--- src/app/users/edit-user/edit-user.component.ts
+++ src/app/users/edit-user/edit-user.component.ts
@@ -42,7 +42,6 @@ export class EditUserComponent implements OnInit {
     /** Create a new form*/
     this.form = this.fb.group({
       firstname: [this.editUser.firstname, Validators.compose([
-        Validators.required,
         Validators.minLength(2),
         Validators.maxLength(32)])
       ],
